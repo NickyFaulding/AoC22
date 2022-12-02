86e3005160ec0386d0373cc031379c399e04c004
Nicky <58389842+NickyFaulding@users.noreply.github.com>
2022-12-02 08:15:10 +0000

    Add project files.

---
+++ 1/Day1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC22
+{
+    public class Day1
+    {
+        private List<int> WeightGroups = new List<int>();
+        private void CalculateWeights()
+        {
+            string[] lines = System.IO.File.ReadAllLines("C:\\Users\\nicky\\source\\repos\\AoC22\\1\\1.txt");
+
+            var groupTotal = 0;
+            foreach (var line in lines)
+            {
+                if (line != String.Empty)
+                {
+                    groupTotal += int.Parse(line);
+                }
+                else
+                {
+                    WeightGroups.Add(groupTotal);
+                    groupTotal = 0;
+                }
+            }
+        }
+
+        private int CalculateTopThree()
+        {
+            List<int> topthree = new List<int>();
+
+            topthree.Add(WeightGroups.Max());
+            WeightGroups.Remove(WeightGroups.Max());
+            topthree.Add(WeightGroups.Max());
+            WeightGroups.Remove(WeightGroups.Max());
+            topthree.Add(WeightGroups.Max());
+
+            return topthree.Sum();
+        }
+
+        public int Part1()
+        {
+            CalculateWeights();
+
+            return WeightGroups.Max();
+        }
+
+        public int Part2()
+        {
+            return CalculateTopThree();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC22
+{
+    internal class AoC22
+    {
+        private static void Main(string[] args)
+        {
+            Day1 day1 = new Day1();
+
+            Console.WriteLine(day1.Part1());
+            Console.WriteLine(day1.Part2());
+        }
+    }
+}
